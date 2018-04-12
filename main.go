e79c54deb0c8c74bf22ed2538bb699df5d505c65
Faaux <c.brammer1@gmail.com>
2018-04-12 18:57:12 +0200

    For Heroku deploly, testing a theory

--- main.go
+++ main.go
@@ -12,10 +12,10 @@ func main(){
 	http.Handle("/query", GraphQL.InitHandler())
 
 	//for local debugging
-	err := http.ListenAndServe(":5000", GraphQL.InitHandler())
+	//err := http.ListenAndServe(":5000", GraphQL.InitHandler())
 
 	//for heroku  usage
-	//err := http.ListenAndServe(":"+os.Getenv("PORT"), nil)
+	err := http.ListenAndServe(":"+os.Getenv("PORT"), nil)
 	if err != nil {
 		panic(err)
 	}
