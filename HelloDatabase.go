1a70c9d9c53891b6e928147dbedd0d13a3aeb5ee
orb1005 <lolilo_07@web.de>
2018-06-11 21:02:42 +0200

    added customer structure

--- HelloDatabase.go
+++ HelloDatabase.go
@@ -3,9 +3,9 @@ package main
 import (
 	_"github.com/lib/pq"
 	"database/sql"
-	"fmt"
-	"log"
 	"os"
+	"log"
+	"fmt"
 )
 
 
@@ -19,14 +19,29 @@ func main() {
 	defer db.Close()
 
 	// Create a test table if it doesnt exist already
-	_,err = db.Query("CREATE TABLE IF NOT EXISTS example (id serial PRIMARY  KEY, name text NOT NULL)")
+	//_,err = db.Query("CREATE TABLE IF NOT EXISTS example (id serial PRIMARY  KEY, name text NOT NULL)")
+	//panicErr(err)
+
+	// Add a user to it
+	//_,err = db.Query("INSERT INTO example(id,name) VALUES($1,$2)", 6,"bast");
+	//panicErr(err)
+
+	_,err = db.Query("CREATE TABLE IF NOT EXISTS customers (id serial PRIMARY KEY, name text NOT NULL, surname text NOT NULL,street text NOT NULL, number integer NOT NULL,zipcode integer NOT NULL,city text NOT NULL,skill integer NOT NULL,email text NOT NULL,telephone text NOT NULL,birthday date NOT NULL)")
 	panicErr(err)
 
 	// Add a user to it
-	_,err = db.Query("INSERT INTO example(id,name) VALUES($1,$2)", 2,"Hans")
-	panicErr(err)
+	//_,err = db.Query("INSERT INTO customers(id,name,surname,street, number,zipcode,city,skill,email,telephone,birthday) VALUES($1,$2,$3,$4,$5,$6,$7,$8,$9,$10,$11)", 4,"seppel","mueller","schulestr",13,81325,"peking",1,"test@test.co.cn","+23413241234","2018-03-15");
+	//panicErr(err)
 
-	rows, err := db.Query("SELECT * FROM example")
+	//remove users
+	//_,err = db.Query("DELETE FROM example WHERE id='5';");
+	//panicErr(err)
+
+	panicErr(err)
+	// Add a user to it
+	_,err = db.Query("UPDATE customers SET name='ching', surname='ling',street='dung'WHERE id = '4';");
+
+	rows, err := db.Query("SELECT * FROM customers")
 	panicErr(err)
 
 	defer rows.Close()
@@ -34,11 +49,20 @@ func main() {
 		var (
 			id string
 			name string
+			surname string
+			street string
+			number int
+			zipcode int
+			city string
+			skill int
+			email string
+			telephone string
+			birthday string
 		)
-		if err := rows.Scan(&id,&name); err != nil {
+		if err := rows.Scan(&id,&name,&surname,&street,&number,&zipcode,&city,&skill,&email,&telephone,&birthday); err != nil {
 			log.Fatal(err)
 		}
-		fmt.Printf("id: %s, name: %s\n", id, name)
+		fmt.Printf("id: %s, name: %s, surname: %s,street: %s,number: %s,zipcode: %s,city: %s,skill: %s,email: %s,telephone: %s,birthday: %s\n", id, name,surname,street,number,zipcode,city,skill,email,telephone,birthday)
 	}
 }
 
---
+++ customer.go
@@ -0,0 +1,129 @@
+package main
+
+import (
+	"time"
+	"os"
+	"database/sql"
+	"log"
+	"fmt"
+)
+
+type Skill int
+
+type Customer struct{
+	id int
+	name  string
+	surname string
+	address Address
+	skill Skill
+	email string
+	telephone string
+	birthday time.Time
+
+}
+
+type Address struct{
+	street string
+	number int
+	zipcode int
+	city string
+}
+
+const (
+	BEGINNER Skill = 0
+	ADVANCED Skill = 1
+	PRO Skill = 2
+)
+
+func Insert(customer *Customer) {
+
+	connStr := os.Getenv("DATABASE_URL")
+	db, err := sql.Open("postgres", connStr)
+	panicErr(err)
+	panicErr(db.Ping()) //Open does not check the connection
+	defer db.Close()
+
+	//create table if it doesn't exist
+	_,err = db.Query("CREATE TABLE IF NOT EXISTS customers (id PRIMARY  KEY, name text NOT NULL, surname text NOT NULL,street text NOT NULL, number integer NOT NULL,zipcode integer NOT NULL,city text NOT NULL,skill integer NOT NULL,email text NOT NULL,telephone text NOT NULL,birthday date NOT NULL)")
+	panicErr(err)
+
+	// Add a user to it
+	_,err = db.Query("INSERT INTO customers(id,name,surname,street, number,zipcode,city,skill,email,telephone,birthday) VALUES($1,$2,$3,$4,$5,$6,$7,$8,$9,$10,$11)",
+		customer.id, customer.name, customer.surname, customer.address.street, customer.address.number, customer.address.zipcode,
+		customer.address.city, customer.skill, customer.email, customer.telephone, customer.birthday);
+	panicErr(err)
+
+	rows, err := db.Query("SELECT * FROM customers")
+	panicErr(err)
+
+	defer rows.Close()
+
+	//additional
+	printDatabase(rows);
+
+}
+
+func Remove(customer *Customer) {
+
+	connStr := os.Getenv("DATABASE_URL")
+	db, err := sql.Open("postgres", connStr)
+	panicErr(err)
+	panicErr(db.Ping()) //Open does not check the connection
+	defer db.Close()
+
+	//remove users
+	_,err = db.Query("DELETE FROM customers WHERE id=customer.id");
+	panicErr(err)
+
+	rows, err := db.Query("SELECT * FROM customers")
+	panicErr(err)
+
+	defer rows.Close()
+
+	//additional
+	printDatabase(rows);
+}
+
+func Update(customer *Customer) {
+
+	connStr := os.Getenv("DATABASE_URL")
+	db, err := sql.Open("postgres", connStr)
+	panicErr(err)
+	panicErr(db.Ping()) //Open does not check the connection
+	defer db.Close()
+
+	// update user in database
+	_,err = db.Query("UPDATE customers SET name=customer.name, surname=customer.surname,street=customer.address.street,number=customer.address.number,zipcode=customer.address.zipcode,city=customer.address.city,skill=customer.skill,email=customer.email,telephone=customer.telephone,birthday=customer.birthday WHERE id = customer.id;");
+	panicErr(err)
+
+	rows, err := db.Query("SELECT * FROM customers")
+	panicErr(err)
+
+	defer rows.Close()
+
+	//additional
+	printDatabase(rows);
+}
+
+func printDatabase(rows *sql.Rows){
+
+	for  rows.Next(){
+		var (
+			id string
+			name string
+			surname string
+			street string
+			number int
+			zipcode int
+			city string
+			skill int
+			email string
+			telephone string
+			birthday string
+		)
+		if err := rows.Scan(&id,&name,&surname,&street,&number,&zipcode,&city,&skill,&email,&telephone,&birthday); err != nil {
+			log.Fatal(err)
+		}
+		fmt.Printf("id: %s, name: %s, surname: %s,street: %s,number: %s,zipcode: %s,city: %s,skill: %s,email: %s,telephone: %s,birthday: %s\n", id, name,surname,street,number,zipcode,city,skill,email,telephone,birthday)
+	}
+}
